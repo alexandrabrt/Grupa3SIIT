cc410bff5b75f61e9f1808eb4aca1f4e207a8628
Alexandra Bratu <alexandra.bratu89@gmail.com>
2020-10-28 19:58:10 +0200

    functii

--- for_ex.py
+++ for_ex.py
@@ -1,153 +1,62 @@
-# for variabila_tempora in functie / lista / string:
-#     pass
-# functie =>> range, enumerate, item
-# for i in range(10):
-#     print(i)
+import sys
+# import judete as j
+# import luni as l
+# import colors as c
 
-# range(pas_inceput, pas_final, pasul)
-#incepe mereu de la 0 daca nu este specificat pasul de inceput
 
-# for i in range(2, 5):
-#     print(i)
+def eroare():
+    print('\n CNP-ul nu este valid')
 
-# for i in range(3, 7, 2):
-#     print(i)
 
-# for i in range(3, 7, -2):
-#     print(i)
+# sys.stdout.write(c.GREEN)
+cnp_test = '279146358279'
+cnp = input('Introduceti CNP-ul Dvs:\n \n')
+cnp_flag = True
 
-# for i in range(10, -11, -2):
-#     print(i)
+# sys.stdout.write(c.RED)
+while cnp_flag:
+    try:
+        type(cnp) == int
+    except ValueError:
+        eroare()
 
-# list_a = [1, 2, 3, 4]
-# for index, value in enumerate(list_a):
-#     print(index, '=>', value)
+    S = cnp[0]
+    AA = cnp[1:3]
+    LL = cnp[3:5]
+    ZZ = cnp[5:7]
+    JJ = cnp[7:9]
+    NNN = cnp[9:12]
 
-# a = "Ana are mere"
-# print(list(a))
-# for i in a:
-#     print(i)
-import datetime
+    if len(cnp) == 13 and int(S) and int(AA) and int(ZZ) and int(LL) and int(JJ) and int(NNN) > 0:
+        pass
+    else:
+        eroare()
+        break
+    if int(LL) < 12 and int(ZZ) < 31 and int(JJ) < 52:
+        pass
+    else:
+        eroare()
+        break
 
-# a = "Ana are mere"
-# print(a[4])
-# print(a[-1])
-# print(a[1:5])
-# print(a[2:])
-# print(len(a)) #lungime
-# print(a[1:5:2])
-# print(a.find('are'))
-# cnp = '1234567890123'
-# an = cnp[1:3]
-# luna = cnp[3:5]
-# zi = cnp[5:7]
-# print(f"19{an}", luna, zi)
-# print(datetime.datetime.strftime())
+    cnp_test_sum = int(cnp[0])*int(cnp_test[0]) + int(cnp[1])*int(cnp_test[1]) + int(cnp[2])*int(cnp_test[2]) + int(cnp[3])*int(cnp_test[3]) + int(cnp[4])*int(cnp_test[4]) + int(cnp[5]) * \
+        int(cnp_test[5]) + int(cnp[6])*int(cnp_test[6]) + int(cnp[7])*int(cnp_test[7]) + \
+        int(cnp[8])*int(cnp_test[8]) + int(cnp[9]) * \
+        int(cnp_test[9]) + int(cnp[10])*int(cnp_test[10]) + \
+        int(cnp[11])*int(cnp_test[11])
 
-# x = "mere"
-# x[0] = 'p'
-# print(x.replace("e", "p"))
-# print(y)
-# print(x.find("e"))
-# a = "ana are mere"
-# print(a.split(" "))
-# print(a.replace(a[0], "e"))
-# a = a.replace(a[0:3], "esdsdsd")
-# print(a)
-# for i, v in enumerate(x):
-#     print(i, v)
-#tuple
-# a = ("re",)
-# print(type(a))
-# a = ("a", False, 4, ("tuple", 3), "soare")
-# print(type(a))
-# y = ("b", 4)
-# print(a+y)
-# for i in y:
-#     print(i)
-# print(y[0])
-# print()
-# y[0] = "a"
+    cnp_test_modulo = cnp_test_sum % 11
 
-#lista
-# x = ["a", False, 4, ("tuple", 3), "soare"]
-# for i in x:
-#     print(i)
-# print(x[-1])
-# x[0] = "b"
-# print(x)
-# print(type(list(a)))
-# x = ["A", True, '4', ["1", "4", False, True, ["1"], ["7", "b", None]]]
-# print(x[3][5][2])
-# y = [[123, False, "abc", "e"], ["1"], "a", True, ["7", "cdef"]]
-# print(y[2])
-# print(y[0][2][1])
-# print(y[4][1][3])
-# print(str(y[0][0])[2])
-# y.append(2)
-# print(y)
-# y.remove("a")
-# y.pop(2)
-# y.insert(2, "alo")
-# print(y[-1])
-# s = {1, 7, 'A', 'a', 3, 4, 5, 5}
-# print(s)
-# # print(type(s))
-# # s.add(6)
-# # print(list(s))
-# # print(s[1])
-# for x in set(s):
-#     print(x)
+    # sys.stdout.write(c.GREEN)
+    if int(cnp[-1]) == cnp_test_modulo:
+        print('\n\n Cnp-ul este Valid!')
+        # sys.stdout.write(c.CYAN)
+        print('\n Sunteti nascut in anul {}, luna {}, in Judetul {}.\n'.format(int(AA), l.luni[int(LL)],
+                                                                               j.judete[int(JJ)]))
+        break
+    else:
+        # sys.stdout.write(c.RED)
+        eroare()
+        cnp_flag = False
+    sys.stdout.write(c.RESET)
 
-# a = {'a', 'b'}
-# a = [1, 2, 3]
-# b = [4, 5, 2]
-# print(set(b) - set(a))
-# s.discard(5)
-# print(s)
-# x = {1}
-# y = {1}
-# ord(str(15))
-# print(x <= y)
-# print(x*3)
-# y = "abc"
-# print(y*3)
-
-# a = {"3": 3, 1: "abv", "2": [1, 2, 3], (1, 2): "a"}
-# if "3" in a:
-#     print('da')
-# a = {"3": 3,
-#      1: "abv",
-#      "2": [1, 2, 3],
-#      (1, 2): "a",
-#      "3": 2}
-# for x in a.keys():
-#     print(x)
-# for x in a.values():
-#     print(x)
-
-# for i, v in a.items():
-    # print(i, '=>', v)
-    # print(a[i])
-
-# print(a[1])
-# print(a)
-# a["2"] = "abc"
-# print(a)
-# a = {"4": 2}
-# print(a)
-# a[2] = "fff"
-# print(a)
-
-# b = [1, 2, 3]
-# if 4 in b:
-#     print('da')
-
-# i = 10
-# while i > 0:
-#     print(i)
-#     i -= 1
-#
-for i in range(10, 0, -1):
-    print(i)
-
+    print(cnp_test_modulo, int(cnp[-1]))
