9735434da3004c1e112a87b17fed06baa50a253c
Alexandra Bratu <alexandra.bratu89@gmail.com>
2020-11-18 21:08:56 +0200

    CLASS

--- class_ex.py
+++ class_ex.py
@@ -88,7 +88,7 @@
 # print(obiect1_stiva.pop())
 # print(obiect1_stiva.pop())
 
-customer = 'AAA'
+# customer = 'AAA'
 
 
 # class User:
@@ -198,18 +198,18 @@ customer = 'AAA'
 # print(hasattr(Clasa1, 'prop'))
 # print(obiect1.method())
 
-class Clasa1:
-
-    varia = 1
-
-    def __init__(self):
-        self.var = 2
-
-    def method(self):
-        pass
-
-    def __hidden(self):
-        pass
+# class Clasa1:
+#
+#     varia = 1
+#
+#     def __init__(self):
+#         self.var = 2
+#
+#     def method(self):
+#         pass
+#
+#     def __hidden(self):
+#         pass
 
     # def metoda1(self):
     #     print('metoda 1')
@@ -219,10 +219,241 @@ class Clasa1:
     #     self.metoda1()
 
 
-obj = Clasa1()
+# obj = Clasa1()
 # print(obj.__dict__)
 # print(Clasa1.__dict__)
 # obj = Clasa1()
-print(Clasa1.__dict__)
+# print(Clasa1.__dict__)
 
 
+# class ExampleClass:
+#     attrx = 1
+#     propxs = 3
+#
+#
+# print(hasattr(ExampleClass, 'attrx'))
+# print(hasattr(ExampleClass, 'propxs'))
+
+
+# class Classy:
+#
+#     variabila = 2
+#
+#     def method_new(self, parametru):
+#         print('metoda', parametru, self.variabila, x.var)
+#
+#
+# x = Classy()
+# x.var = 3
+# x.method_new(1)
+# x.method_new(2)
+# x.method_new(3)
+
+
+# class Classy:
+#
+#     def __init__(self, value='222'):
+#         # pass
+#         self.var = value
+#
+#     def __str__(self):
+#         return self.var
+#
+#
+# obj1 = Classy("object")
+# print(obj1)
+# print(obj1.var)
+
+
+# class Classy:
+#
+#     def visible(self):
+#         print('visible')
+#
+#     def __hidden(self):
+#         print('hidden')
+#
+#
+# obj1 = Classy()
+# # obj1.visible()
+# # obj1.__hidden()
+#
+# try:
+#     obj1.__hidden()
+# except Exception:
+#     print('failed')
+#
+#
+# obj1._Classy__hidden()
+# obj1.visible()
+
+#
+# class Classy:
+#
+#     variabila = 1
+#
+#     def __init__(self):
+#         self.var = 2
+#
+#     def method(self):
+#         pass
+#
+#     def __hidden(self):
+#         pass
+#
+#
+# obj1 = Classy()
+# # print(obj1.__dict__)
+# # print(Classy.__dict__)
+# # print(Classy.__name__)
+# print(type(obj1).__name__)
+
+
+# class SuperOne:
+#     pass
+#
+#
+# class SuperTwo:
+#     pass
+#
+#
+# class SuperThree(SuperOne, SuperTwo):
+#     pass
+#
+#
+# def print_bases(cls):
+#     for x in cls.__bases__:
+#         print(x.__name__)
+#     return True
+#
+#
+# print_bases(SuperOne)
+# print_bases(SuperTwo)
+# print_bases(SuperThree)
+
+
+# class Star:
+#
+#     def __init__(self, name, galaxy):
+#         self.name = name
+#         self.galaxy = galaxy
+#
+#     def __str__(self):
+#         return f"{self.name} {self.galaxy}"
+#
+#
+# sun = Star("Sun", "Milky Way")
+# print(sun)
+
+
+# class Vehicule:
+#     pass
+#
+#
+# class VehiculeHibride(Vehicule):
+#     pass
+#
+#
+# class VehiculeElectrice(VehiculeHibride):
+#     pass
+#
+#
+# vehicule_obj = Vehicule()
+# vehicule_hibride_obj = VehiculeHibride()
+# vehicule_electrice_obj = VehiculeElectrice()
+
+# for cls1 in [Vehicule, VehiculeHibride, VehiculeElectrice]:
+#     for cls2 in [Vehicule, VehiculeHibride, VehiculeElectrice]:
+#         print(cls1.__name__, cls2.__name__)
+#         print(issubclass(cls1, cls2), end='\t')
+#     print()
+
+# print(issubclass(VehiculeElectrice, Vehicule))
+# print(isinstance(vehicule_hibride_obj, Vehicule))
+
+
+# class Classy:
+#     def __init__(self, val):
+#         self.var = val
+#
+#
+# obj1 = Classy(0)
+# obj2 = Classy(2)
+# obj3 = obj1
+
+# print(obj1 is obj2)
+# print(obj2 is obj3)
+# print(obj3 is obj1)
+
+# str1 = 'Ana are mere '
+# str2 = 'Ana are mere si pere'
+# str1 += 'si pere'
+
+# print(str1 == str2)
+# print(str1 is str2)
+
+
+# int1 = 1.0
+# int2 = 2.0
+# int1 += 1.0
+#
+# print(int1 == int2)
+# print(int1 is int2)
+
+
+# class SuperClass:
+#     super_variabila = 1
+#
+#     def __init__(self, name):
+#         self.name = name
+#
+#     def __str__(self):
+#         return f'Numele meu este {self.name}'
+#
+#
+# class SubClass(SuperClass):
+#     sub_variabila = 2
+#     # super_variabila = 3
+#
+#     def __init__(self, name, varsta=11):
+#         self.varsta = varsta
+#         # SuperClass.__init__(self, name)
+#         super().__init__(name)
+#
+#     def __str__(self):
+#         return f"Ma numesc"
+#
+#
+# obj = SubClass('Ovidiu')
+# print(obj.super_variabila)
+# obj1 = SuperClass('Popescu')
+
+
+class SuperClass:
+    def __init__(self):
+        self.super_variabila = 11
+    def metoda1(self):
+        return 'superclass'
+class MiddleClass(SuperClass):
+    def __init__(self):
+        super().__init__()
+        self.super_variabila = 13
+        # print(self.super_variabila)
+class SubClass(MiddleClass):
+    def __init__(self):
+        super().__init__()
+        # super(SubClass, self).__init__()
+        self.sub_variabila = 12
+        self.super_variabila = 40
+        # print(self.super_variabila)
+
+        # self.super_variabila = 111
+    def metoda1(self):
+        return 'subclasa'
+
+    def __str__(self):
+        return f"{self.metoda1()} 1"
+obj = SubClass()
+# print(obj.sub_variabila)
+print(obj.metoda1())
+# print(obj.metoda1())
--- functii.py
+++ functii.py
@@ -1,56 +1,58 @@
-# print()
-
-# def message():
-#     print('Numarul introdus este 2')
-
-
-# message()
-# print(message())
-# message = 1
-# print(message)
-
-
-# def message(numar_2=1, numar_1=10):
-#     print(f'Numarul introdus este {numar_2} si al doilea numar este {numar_1}')
+# # print()
+#
+# # def message():
+# #     print('Numarul introdus este 2')
 #
 #
-# message()
-
-
-# def suma(a: int, c: int, b: int = 2) -> (list, int):
-#     """
-#     :param a:
-#     :param c:
-#     :param b:
-#     :return: lista de suma, diferenta
-#     """
-#     return [a + b + c], a - b - c
+# # message()
+# # print(message())
+# # message = 1
+# # print(message)
 #
 #
-# valoare_suma, valoare_dif = suma(10, 2)
-# print(valoare_suma, valoare_dif)
-
-
-# def inmultire(a: int, b: int) -> (bool, int):
-#     rezultat = a * b
-#     return True, rezultat
+# # def message(numar_2=1, numar_1=10):
+# #     print(f'Numarul introdus este {numar_2} si al doilea numar este {numar_1}')
+# #
+# #
+# # message()
 #
 #
-# print(inmultire(1, 2))
+# # def suma(a: int, c: int, b: int = 2) -> (list, int):
+# #     """
+# #     :param a:
+# #     :param c:
+# #     :param b:
+# #     :return: lista de suma, diferenta
+# #     """
+# #     return [a + b + c], a - b - c
+# #
+# #
+# # valoare_suma, valoare_dif = suma(10, 2)
+# # print(valoare_suma, valoare_dif)
+#
+#
+# # def inmultire(a: int, b: int) -> (bool, int):
+# #     rezultat = a * b
+# #     return True, rezultat
+# #
+# #
+# # print(inmultire(1, 2))
+#
+# def listare(a: bool, b: bool) -> bool:
+#     if a is True:
+#         return True
+#     if b is False:
+#         return False
+#
+#
+# print(listare(False, False))
+#
+#
+# def printare(a, b):
+#     listare(a, b)
+#     return 'S-a executat cu succes'
+#
+#
+# print(printare(True, False))
 
-def listare(a: bool, b: bool) -> bool:
-    if a is True:
-        return True
-    if b is False:
-        return False
 
-
-print(listare(False, False))
-
-
-def printare(a, b):
-    listare(a, b)
-    return 'S-a executat cu succes'
-
-
-print(printare(True, False))
