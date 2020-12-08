5b15f08a8fd6c97a5bae3475ba687fe1a60285ef
Alexandra Bratu <alexandra.bratu89@gmail.com>
2020-12-08 21:43:06 +0200

    DJANGO v2

--- proiect/aplicatie1/urls.py
+++ proiect/aplicatie1/urls.py
@@ -5,5 +5,6 @@ from aplicatie1 import views
 app_name = 'aplicatie1'
 
 urlpatterns = [
-    path('', views.HomeIndex.as_view(), name="home")
+    path('', views.HomeIndex.as_view(), name="home"),
+    path('add_location/', views.CreateIndexView.as_view(), name="adaugare")
 ]
--- proiect/aplicatie1/views.py
+++ proiect/aplicatie1/views.py
@@ -5,12 +5,22 @@ from django.shortcuts import render
 # CreateView => Adaugare date
 # UpdateView => Modificare date
 # DeleteView => Stergere date
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 
 from aplicatie1.models import Location
+from django.urls import reverse
 
 
 class HomeIndex(ListView):
     model = Location
     template_name = 'aplicatie1/location_index.html'
     context_object_name = 'all_locations'
+
+
+class CreateIndexView(CreateView):
+    model = Location
+    fields = ['city', 'country']
+    template_name = 'aplicatie1/location_form.html'
+
+    def get_success_url(self):
+        return reverse('aplicatie1:home')
