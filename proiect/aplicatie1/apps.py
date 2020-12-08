b4a5753b386c3b8a4f35799f5232bb11c8d722c6
Alexandra Bratu <alexandra.bratu89@gmail.com>
2020-12-08 19:15:23 +0200

    django ex1

---
+++ proiect/aplicatie1/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Aplicatie1Config(AppConfig):
+    name = 'aplicatie1'
---
+++ proiect/aplicatie2/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Aplicatie2Config(AppConfig):
+    name = 'aplicatie2'
